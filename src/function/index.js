4d6222fbf49f4aaf2f0230dbed5707f1f51f0409
diodev2023 <dio.dev2023@gmail.com>
2023-10-28 11:22:40 -0300

    segundo desafio do Felipão

---
+++ src/function/index.js
@@ -0,0 +1,7 @@
+torrar("pão de forma" , "do Rodrigo")
+torrar("pão integral" , "da Gislene")
+
+function torrar(pao, nome){
+    console.log("torrada feita com " + pao)
+    console.log("ela é um pedido " + nome)
+}
