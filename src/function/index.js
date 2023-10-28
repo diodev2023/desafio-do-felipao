b96e9698b5879c24ca59ad49b57f666281a41343
diodev2023 <dio.dev2023@gmail.com>
2023-10-28 12:55:19 -0300

    excluíndo aquivo desnecessario

--- src/function/index.js
+++
@@ -1,7 +0,0 @@
-torrar("pão de forma" , "do Rodrigo")
-torrar("pão integral" , "da Gislene")
-
-function torrar(pao, nome){
-    console.log("torrada feita com " + pao)
-    console.log("ela é um pedido " + nome)
-}
